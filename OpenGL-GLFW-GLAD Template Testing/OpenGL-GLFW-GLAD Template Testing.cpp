fc1aee0d82a968fe8c07aa2aa6411e4dfba97ad0
Griffin Nelson <griffin.nelson@snhu.edu>
2021-05-07 17:45:55 -0400

    Added CreateGLFWWindow test

--- OpenGL-GLFW-GLAD Template Testing/OpenGL-GLFW-GLAD Template Testing.cpp
+++ OpenGL-GLFW-GLAD Template Testing/OpenGL-GLFW-GLAD Template Testing.cpp
@@ -3,11 +3,29 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
+#include "main.h"
+#include "events.h"
+
 namespace OpenGLGLFWGLADTemplateTesting
 {
 	TEST_CLASS(OpenGLGLFWGLADTemplateTesting)
 	{
 	public:
-		
-	};
+
+		TEST_METHOD(CreateGLFWWindow)
+		{
+			int window_width = 0,
+				window_height = 0;
+
+			GLFWwindow* window = create_glfw_window();
+			Assert::IsNotNull(window, L"Window not created");
+
+			glfwGetFramebufferSize(window, &window_width, &window_height);
+			Assert::AreEqual(GLFW_WINDOW_WIDTH, window_width, L"Unexpected window width");
+			Assert::AreEqual(GLFW_WINDOW_HEIGHT, window_height, L"Unexpected window height");
+
+			glfwTerminate();
+		}
+	}
 }
+
