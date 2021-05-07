82351f3dd8cb531c2f9bd784bc4e598c438cf935
Griffin Nelson <griffin.nelson@snhu.edu>
2021-05-07 17:45:18 -0400

    Added Testing solution skeleton

---
+++ OpenGL-GLFW-GLAD Template Testing/OpenGL-GLFW-GLAD Template Testing.cpp
@@ -0,0 +1,13 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace OpenGLGLFWGLADTemplateTesting
+{
+	TEST_CLASS(OpenGLGLFWGLADTemplateTesting)
+	{
+	public:
+		
+	};
+}
